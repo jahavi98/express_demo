047b8bd889e7318153cef61935213ab62ed40a0e
jahnavi98 <jahnavi.bhagat.hs@gmail.com>
2021-11-25 11:02:36 +0530

    diva commit

--- controllers/api/category.controller.js
+++ controllers/api/category.controller.js
@@ -2,7 +2,7 @@ const models = require("../../models");
 const Category = models.Category;
 const Op = models.Sequelize.Op;
 
-// Create and Save a new product
+// Create and Save a new category
 exports.create = (req, res) => {
 
     if (!req.body.category) {
@@ -12,14 +12,14 @@ exports.create = (req, res) => {
         return;
     }
 
-    // Create a Product
+    // Create a Category
     const category = {
         category: req.body.category,
         status: req.body.status,
         image:req.body.image,
     };
 
-    // Save Product in the database
+    // Save Catgeory in the database
     Category.create(category)
         .then(data => {
             res.send(data);
@@ -27,17 +27,18 @@ exports.create = (req, res) => {
         .catch(err => {
             res.status(500).send({
                 message:
-                    err.message || "Some error occurred while creating the Product."
+                    err.message || "Some error occurred while creating the category."
             });
         });
 };
 
-// Retrieve all Products from the database.
+// Retrieve all Category from the database.
 exports.findAll = (req, res) => {
-    const name = req.query.name;
-    // var condition = name ? { name: { [Op.like]: `%${name}%` } } : null;
-
+    const category = req.query.category;
     Category.findAll({
+    where:{
+              isdelete:0
+    },
         raw:true
     }).then(data => {
         res.send(data);
@@ -45,12 +46,12 @@ exports.findAll = (req, res) => {
         .catch(err => {
             res.status(500).send({
                 message:
-                    err.message || "Some error occurred while retrieving products."
+                    err.message || "Some error occurred while retrieving category."
             });
         });
 };
 
-// Find a single product with an id
+// Find a single category with an id
 exports.findOne = (req, res) => {
     const id = req.params.id;
 
@@ -60,43 +61,42 @@ exports.findOne = (req, res) => {
                 res.send(data);
             } else {
                 res.status(404).send({
-                    message: `Cannot find Products with id=${id}.`
+                    message: `Cannot find category with id=${id}.`
                 });
             }
         })
         .catch(err => {
             res.status(500).send({
-                message: "Error retrieving products with id=" + id
+                message: "Error retrieving category with id=" + id
             });
         });
 };
 
-// Update a product by the id in the request
+// Update a category by the id in the request
 exports.update = (req, res) => {
     const id = req.params.id;
-
     Category.update(req.body, {
         where: { id: id }
     })
         .then(id => {
             if (id == 1) {
                 res.send({
-                    message: "Product was updated successfully."
+                    message: "Category was updated successfully."
                 });
             } else {
                 res.send({
-                    message: `Cannot update Product with id=${id}. Maybe Product was not found or req.body is empty!`
+                    message: `Cannot update Category with id=${id}. Maybe Category was not found or req.body is empty!`
                 });
             }
         })
         .catch(err => {
             res.status(500).send({
-                message: "Error updating Product with id=" + id
+                message: "Error updating Category with id=" + id
             });
         });
 };
 
-// Delete a product with the specified id in the request
+// Delete a category with the specified id in the request
 exports.delete = (req, res) => {
     const id = req.params.id;
 
@@ -110,39 +110,39 @@ exports.delete = (req, res) => {
         .then(id => {
             if (id == 1) {
                 res.send({
-                    message: "Product was deleted successfully!"
+                    message: "Category was deleted successfully!"
                 });
             } else {
                 res.send({
-                    message: `Cannot delete product with id=${id}. Maybe product was not found!`
+                    message: `Cannot delete category with id=${id}. Maybe category was not found!`
                 });
             }
         })
         .catch(err => {
             res.status(500).send({
-                message: "Could not delete product with id=" + id
+                message: "Could not delete category with id=" + id
             });
         });
 };
 
-// Delete all Products from the database.
+// Delete all Category from the database.
 exports.deleteAll = (req, res) => {
     Category.destroy({
         where: {},
         truncate: false
     })
         .then(id => {
-            res.send({ message: `${id} Products were deleted successfully!` });
+            res.send({ message: `${id} Category were deleted successfully!` });
         })
         .catch(err => {
             res.status(500).send({
                 message:
-                    err.message || "Some error occurred while removing all products."
+                    err.message || "Some error occurred while removing all category."
             });
         });
 };
 
-// Find all published Products
+// Find all published Category
 exports.findAllPublished = (req, res) => {
     Category.findAll({ where: { published: true } })
         .then(data => {
@@ -151,7 +151,7 @@ exports.findAllPublished = (req, res) => {
         .catch(err => {
             res.status(500).send({
                 message:
-                    err.message || "Some error occurred while retrieving products."
+                    err.message || "Some error occurred while retrieving Category."
             });
         });
 };
--- controllers/api/product.controller.js
+++ controllers/api/product.controller.js
@@ -1,9 +1,11 @@
 const models = require("../../models");
+const path = require("path");
+const fs = require("fs");
 const Products = models.Products;
 const Op = models.Sequelize.Op;
 
 // Create and Save a new product
-exports.create = (req, res) => {
+exports.create = async (req, res) => {
 
   if (!req.body.name) {
     res.status(400).send({
@@ -18,24 +20,137 @@ exports.create = (req, res) => {
     pnumber: req.body.pnumber,
     price: req.body.price,
     description: req.body.description,
-    image:req.body.image,
-    category:req.body.category,
-    status:req.body.status,
-    start_date:req.body.start_date,
-    end_date:req.body.end_date
+    imgconvert:req.body.imgconvert,
+    image: req.body.image,
+    status: req.body.status,
+    start_date: req.body.start_date,
+    end_date: req.body.end_date
   };
 
   // Save Product in the database
-  Products.create(product)
-    .then(data => {
+  if (!req.files) {
+    //save product data
+    let {
+      name,
+      pnumber,
+      description,
+      imgconvert,
+      image,
+      category,
+      price,
+      start_date,
+      end_date,
+      status
+    } = await req.body;
+
+    const product = await Products.update({
+      is_deleted: 1,
+      pnumber: 'DEL_' + pnumber
+    }, {
+      where: {
+        pnumber: pnumber
+      }
+    }).then(function (product) {
+      res.status(200);
+    });
+
+    Products.create({
+      name, pnumber, description, imgconvert, image, category, price, start_date, end_date, status
+    }).then(data => {
       res.send(data);
     })
-    .catch(err => {
-      res.status(500).send({
-        message:
-          err.message || "Some error occurred while creating the Product."
-      });
+        .catch(err => {
+          res.status(500).send({
+            message:
+                err.message || "Some error occurred while creating the Product."
+          });
+        });
+  } else {
+    let {
+      name,
+      pnumber,
+      description,
+      imgconvert,
+      image,
+      category,
+      price,
+      start_date,
+      end_date,
+      status
+    } = await req.body;
+    const product = await Products.update({
+      is_deleted: 1,
+      pnumber: 'DEL_' + pnumber
+    }, {
+      where: {
+        pnumber: pnumber
+      }
+    }).then(function (product) {
+      res.status(200);
     });
+
+    let targetFile = req.files.image;
+    let extName = path.extname(targetFile.name);
+    let baseName = path.basename(targetFile.name, extName);
+    let uploadDir = path.join(__dirname, '../../public/images/', targetFile.name);
+    let upload = path.join(__dirname, '../../public/tmp/', targetFile.name);
+
+
+    let imgList = ['.png', '.jpg', '.jpeg', '.gif'];
+
+// Checking the file type
+    if (!imgList.includes(extName)) {
+      fs.unlinkSync(targetFile.tempFilePath);
+      return res.status(422).send("Invalid Image");
+    }
+
+    if (targetFile.size > 1048576) {
+      fs.unlinkSync(targetFile.tempFilePath);
+      console.log("target file", targetfile.size)
+      return res.status(413).send("File is too Large");
+    }
+//save original image in public/images
+    targetFile.mv(uploadDir, (err) => {
+      if (err) {
+        console.log("targetfile err", err)
+        return res.status(500).send(err);
+      }
+    });
+
+//save image name in database
+    image = baseName + extName;
+
+//for replace file name & store in folder public/tmp
+    let timestamp = new Date().getTime();
+    upload = upload.replace(baseName, timestamp);
+
+//for new name file store
+    let convert = timestamp + extName;
+
+    targetFile.mv(upload, (err) => {
+      if (err) {
+        console.log("targetfile err", err)
+        return res.status(500).send(err);
+      }
+    });
+
+//save imgconvert name in the database
+    imgconvert = convert;
+    console.log("************", imgconvert)
+
+//all data save in database
+    Products.create({
+      name, pnumber, description, imgconvert, image, category, price, start_date, end_date, status
+    }).then(data => {
+      res.send(data);
+    })
+        .catch(err => {
+          res.status(500).send({
+            message:
+                err.message || "Some error occurred while creating the Product."
+          });
+        });
+  }
 };
 
 // Retrieve all Products from the database.
